55abea5a7119e1f62232ab14a92193d49e36e31a
linm <1463169321@qq.com>
2023-04-01 22:07:09 +0800

    master commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello word!");
         System.out.println("hello word3!");
         System.out.println("我是hot-fix分支");
+        System.out.println("master test");
     }
 }

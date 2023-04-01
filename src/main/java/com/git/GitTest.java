f041de76ee7769c388d996d33570b18ca2d79127
linmyx <103361241+linmyx@users.noreply.github.com>
2023-04-01 22:25:04 +0800

    github commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("hot-fix test");
         System.out.println("first git push");
+        System.out.println("hello github");
     }
 }

fa0313fa0b361e93e992f4e5dfd4494eb8d78784
linm <1463169321@qq.com>
2023-04-01 22:21:42 +0800

    first git commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -1,14 +1,18 @@
 package com.git;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class GitTest {
     public static void main(String[] args) {
-
         System.out.println("hello git!");
         System.out.println("hello word!");
         System.out.println("hello word3!");
         System.out.println("我是hot-fix分支");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("first git push");
     }
 }
